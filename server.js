3078d8b7ad8746bb6b631e41ba7865a81435e19a
charan <75157318+charan8340@users.noreply.github.com>
2024-09-05 10:36:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path'); // Import path module
 
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files (e.g., your HTML file)
 app.use(express.static(path.join(__dirname, 'public')));
